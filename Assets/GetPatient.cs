45776ec2b1501f64526b7c4a646df304071d4dcb
Beatriz Oliveira <beatrizrosaoliveira03@gmail.com>
2023-11-21 15:16:04 +0000

    3. Matching Agents with Agents

--- Assets/GetPatient.cs
+++ Assets/GetPatient.cs
@@ -6,6 +6,9 @@ public class GetPatientl : GAction
 {
    public override bool PrePerform()
     {
+        target = GWorld.Instance.RemovePatient();
+        if (target == null)
+            return false;
         return true;
     }
     public override bool PostPerform()
